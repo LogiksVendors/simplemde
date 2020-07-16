0d1c3c72e34664c6a3f817cb5322667f1ce51739
SILK Admin <admin@smartinfologiks.com>
2020-07-16 09:47:20 +0000

    First commit

---
+++ boot.php
@@ -0,0 +1,8 @@
+<?php
+if(!defined('ROOT')) exit('No direct script access allowed');
+
+$wPath=dirname(getWebPath(__FILE__))."/";
+//echo $wPath;
+?>
+<link href='<?=$wPath?>simplemde/simplemde.min.css' rel='stylesheet' type='text/css' />
+<script src='<?=$wPath?>simplemde/simplemde.min.js' type='text/javascript' language='javascript'>
